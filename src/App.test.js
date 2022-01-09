e97121040946c8f9c1471950effbe6a7cabed669
Vivek Kumar <vivekkumar091195@gmail.com>
2022-01-09 22:22:23 +0530

    Added a failed test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders the text properly", () => {
   render(<App />);
   const linkElement = screen.getByText(/world of Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
